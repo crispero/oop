ec805b6fa607bc9e90e2eb62f1407e8e566b8daf
Igor Taigin <solitudesunshine@gmail.com>
2019-05-30 11:19:04 +0300

    доделал CRational

---
+++ lab5/Rational/Const.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+
+const std::string DENOMINATOR_MUST_NOT_BE_ZERO = "Denominator must not be equal to zero";
+const std::string NOT_RATIONAL_NUMBER = "This is not a rational number";
--- lab5/RationalTest/RationalTest/RationalTest.cpp
+++ lab5/RationalTest/RationalTest/RationalTest.cpp
@@ -2,7 +2,7 @@
 #include "lab5/Rational/CRational.h"
 #include <catch2/catch.hpp>
 
-TEST_CASE("")
+TEST_CASE("Methods")
 {
 	SECTION("Check init")
 	{
@@ -43,6 +43,13 @@ TEST_CASE("")
 		CRational rational10(450, 3000);
 		CHECK(rational10.GetNumerator() == 3);
 		CHECK(rational10.GetDenominator() == 20);
+
+		CRational rational11(6);
+		CHECK(rational11.GetNumerator() == 6);
+		CHECK(rational11.GetDenominator() == 1);
+
+		CHECK(CRational().GetNumerator() == 0);
+		CHECK(CRational().GetDenominator() == 1);
 	}
 
 	SECTION("To double")
@@ -68,18 +75,14 @@ TEST_CASE("")
 		CHECK(GreatestCommonDenominator(49, 7) == 7);
 		CHECK(GreatestCommonDenominator(22, 11) == 11);
 		CHECK(GreatestCommonDenominator(50, 5) == 5);
-		CHECK(GreatestCommonDenominator(0.2, 5) == 5);
-		CHECK(GreatestCommonDenominator(5.2, 5) == 5);
-		CHECK(GreatestCommonDenominator(5.2, 6) == 1);
-		CHECK(GreatestCommonDenominator(4.4, 2.2) == 2);
 		CHECK(GreatestCommonDenominator(-1, 2) == 1);
 		CHECK(GreatestCommonDenominator(1, -2) == 1);
 	}
 }
 
-TEST_CASE("operators")
+TEST_CASE("Operators")
 {
-	SECTION("unary + ")
+	SECTION("Unary + ")
 	{
 		CRational rational(1, 2);
 		CRational rationalResult = +rational;
@@ -92,7 +95,7 @@ TEST_CASE("operators")
 		CHECK(rationalResult1.GetDenominator() == 41);
 	}
 
-	SECTION("unary - ")
+	SECTION("Unary - ")
 	{
 		CRational rational(1, 2);
 		CRational rationalResult = -rational;
@@ -110,7 +113,7 @@ TEST_CASE("operators")
 		CHECK(rationalResult2.GetDenominator() == 3);
 	}
 
-	SECTION("binary + ")
+	SECTION("Binary + ")
 	{
 		CRational rational1(1, 3);
 		CRational rational2(1, 3);
@@ -130,15 +133,316 @@ TEST_CASE("operators")
 		CHECK(rationalResult3.GetNumerator() == 7);
 		CHECK(rationalResult3.GetDenominator() == 6);
 
-		CRational rational5(6, 7);
-		CRational rational6(6, 4);
-		CRational rationalResult3 = rational5 + rational6;
-		CHECK(rationalResult3.GetNumerator() == 7);
-		CHECK(rationalResult3.GetDenominator() == 6);
+		CRational rational7(6, 7);
+		CRational rational8(6);
+		CRational rationalResult4 = rational7 + rational8;
+		CHECK(rationalResult4.GetNumerator() == 48);
+		CHECK(rationalResult4.GetDenominator() == 7);
+
+		CRational rationalResult5 = rational8 + rational7;
+		CHECK(rationalResult5.GetNumerator() == 48);
+		CHECK(rationalResult5.GetDenominator() == 7);
+
+		
 	}
 
-	SECTION("binary - ")
+	SECTION("Binary - ")
 	{
+		CRational rational1(1, 3);
+		CRational rational2(1, 3);
+		CRational rationalResult = rational1 - rational2;
+		CHECK(rationalResult.GetNumerator() == 0);
+		CHECK(rationalResult.GetDenominator() == 1);
 
+		CRational rational3(5, 4);
+		CRational rational4(6, 5);
+		CRational rationalResult2 = rational3 - rational4;
+		CHECK(rationalResult2.GetNumerator() == 1);
+		CHECK(rationalResult2.GetDenominator() == 20);
+
+		CRational rational5(-1, 3);
+		CRational rational6(6, 4);
+		CRational rationalResult3 = rational5 - rational6;
+		CHECK(rationalResult3.GetNumerator() == -11);
+		CHECK(rationalResult3.GetDenominator() == 6);
+
+		CRational rational7(6, 7);
+		CRational rational8(6);
+		CRational rationalResult4 = rational7 - rational8;
+		CHECK(rationalResult4.GetNumerator() == -36);
+		CHECK(rationalResult4.GetDenominator() == 7);
+
+		CRational rationalResult5 = rational8 - rational7;
+		CHECK(rationalResult5.GetNumerator() == 36);
+		CHECK(rationalResult5.GetDenominator() == 7);
+	}
+
+	SECTION("Operator +=")
+	{
+		CRational rational1(1, 2);
+		CRational rational2(1, 6);
+		rational1 += rational2;
+		CHECK(rational1.GetNumerator() == 2);
+		CHECK(rational1.GetDenominator() == 3);
+
+		CRational rational3(1);
+		CRational rational4(1, 2);
+		rational4 += rational3;
+		CHECK(rational4.GetNumerator() == 3);
+		CHECK(rational4.GetDenominator() == 2);
+
+		CRational rational5(4, 3);
+		CRational rational6(5, 3);
+		rational5 += rational6;
+		CHECK(rational5.GetNumerator() == 3);
+		CHECK(rational5.GetDenominator() == 1);
+
+		CRational rational0(1, 2);
+		CRational rational00(1, 2);
+		rational0 -= rational00;
+		CHECK(rational0.GetNumerator() == 0);
+		CHECK(rational0.GetDenominator() == 1);
+	}
+
+	SECTION("Operator -=")
+	{
+		CRational rational1(1, 2);
+		CRational rational2(1, 6);
+		rational1 -= rational2;
+		CHECK(rational1.GetNumerator() == 1);
+		CHECK(rational1.GetDenominator() == 3);
+
+		CRational rational3(1);
+		CRational rational4(1, 2);
+		rational4 -= rational3;
+		CHECK(rational4.GetNumerator() == -1);
+		CHECK(rational4.GetDenominator() == 2);
+	}
+
+	SECTION("Operator *")
+	{
+		CRational rational1(1, 2);
+		CRational rational2(2, 3);
+		CRational rationalResult = rational1 * rational2;
+		CHECK(rationalResult.GetNumerator() == 1);
+		CHECK(rationalResult.GetDenominator() == 3);
+
+		CRational rational3(1, 2);
+		CRational rational4(-3);
+		CRational rationalResult1 = rational3 * rational4;
+		CHECK(rationalResult1.GetNumerator() == -3);
+		CHECK(rationalResult1.GetDenominator() == 2);
+
+		CRational rational5(7);
+		CRational rational6(2, 3);
+		CRational rationalResult2 = rational5 * rational6;
+		CHECK(rationalResult2.GetNumerator() == 14);
+		CHECK(rationalResult2.GetDenominator() == 3);
+
+		CRational rational9(4, 3);
+		CRational rational10(3, 3);
+		CRational rationalResult6 = rational9 * rational10;
+		CHECK(rationalResult6.GetNumerator() == 4);
+		CHECK(rationalResult6.GetDenominator() == 3);
+	}
+
+	SECTION("Operator /")
+	{
+		CRational rational1(1, 2);
+		CRational rational2(2, 3);
+		CRational rationalResult = rational1 / rational2;
+		CHECK(rationalResult.GetNumerator() == 3);
+		CHECK(rationalResult.GetDenominator() == 4);
+
+		CRational rational3(1, 2);
+		CRational rational4(5);
+		CRational rationalResult1 = rational3 / rational4;
+		CHECK(rationalResult1.GetNumerator() == 1);
+		CHECK(rationalResult1.GetDenominator() == 10);
+
+		CRational rational5(7);
+		CRational rational6(2, 3);
+		CRational rationalResult2 = rational5 / rational6;
+		CHECK(rationalResult2.GetNumerator() == 21);
+		CHECK(rationalResult2.GetDenominator() == 2);
+	}
+
+	SECTION("Operator *=")
+	{
+		CRational rational1(1, 2);
+		CRational rational2(2, 3);
+		rational1 *= rational2;
+		CHECK(rational1.GetNumerator() == 1);
+		CHECK(rational2.GetDenominator() == 3);
+
+		CRational rational3(1, 2);
+		CRational rational4(3);
+		rational3 *= rational4;
+		CHECK(rational3.GetNumerator() == 3);
+		CHECK(rational3.GetDenominator() == 2);
+	}
+
+	SECTION("Operator /=")
+	{
+		CRational rational1(1, 2);
+		CRational rational2(2, 3);
+		rational1 /= rational2;
+		CHECK(rational1.GetNumerator() == 3);
+		CHECK(rational1.GetDenominator() == 4);
+
+		CRational rational3(1, 2);
+		CRational rational4(3);
+		rational3 /= rational4;
+		CHECK(rational3.GetNumerator() == 1);
+		CHECK(rational3.GetDenominator() == 6);
+
+		CRational rational5(3, 4);
+		CRational rational6(3, 8);
+		rational5 /= rational6;
+		CHECK(rational5.GetNumerator() == 2);
+		CHECK(rational5.GetDenominator() == 1);
+	}
+
+	SECTION("Operator ==")
+	{
+		CRational rational1(1, 2);
+		CRational rational2(1, 2);
+		bool result = (rational1 == rational2);
+		CHECK(result == true);
+
+		CRational rational3(1, 2);
+		CRational rational4(2, 3);
+		bool result1 = (rational3 == rational4);
+		CHECK(result1 == false);
+
+		CRational rational5(4, 1);
+		CRational rational6(4);
+		bool result2 = (rational5 == rational6);
+		CHECK(result2 == true);
+
+		CRational rational7(1, 2);
+		CRational rational8(7);
+		bool result3 = (rational7 == rational8);
+		CHECK(result3 == false);
+
+		CRational rational9(3);
+		CRational rational10(3, 1);
+		bool result4 = (rational9 == rational10);
+		CHECK(result4 == true);
+
+		CRational rational11(3);
+		CRational rational12(2, 3);
+		bool result5 = (rational11 == rational12);
+		CHECK(result5 == false);
+	}
+
+	SECTION("Operator !=")
+	{
+		CRational rational1(1, 2);
+		CRational rational2(1, 2);
+		bool result = (rational1 != rational2);
+		CHECK(result == false);
+
+		CRational rational3(1, 2);
+		CRational rational4(2, 3);
+		bool result1 = (rational3 != rational4);
+		CHECK(result1 == true);
+
+		CRational rational5(4, 1);
+		CRational rational6(4);
+		bool result2 = (rational5 != rational6);
+		CHECK(result2 == false);
+
+		CRational rational7(1, 2);
+		CRational rational8(7);
+		bool result3 = (rational7 != rational8);
+		CHECK(result3 == true);
+
+		CRational rational9(3);
+		CRational rational10(3, 1);
+		bool result4 = (rational9 != rational10);
+		CHECK(result4 == false);
+
+		CRational rational11(3);
+		CRational rational12(2, 3);
+		bool result5 = (rational11 != rational12);
+		CHECK(result5 == true);
+	}
+
+	SECTION("Operators <, <=, >, >=")
+	{
+		CRational rational1(1, 2);
+		CRational rational2(1, 3);
+		bool result = (rational1 >= rational2);
+		CHECK(result == true);
+
+		bool result1 = (rational1 <= rational2);
+		CHECK(result1 == false);
+
+		CRational rational3(3, 1);
+		CRational rational4(2);
+		bool result2 = (rational3 > rational4);
+		CHECK(result2 == true);
+
+		CRational rational5(7);
+		bool result3 = (rational1 < rational5);
+		CHECK(result3 == true);
+
+		CRational rational6(3);
+		CRational rational7(7, 2);
+		bool result4 = (rational6 <= rational7);
+		CHECK(result4 == true);
+
+		CRational rational8(8, 2);
+		bool result5 = (rational6 >= rational8);
+		CHECK(result5 == false);
+	}
+
+	SECTION("Operator <<")
+	{
+		CRational rational1(7, 15);
+		std::stringstream output1;
+		output1 << rational1;
+		CHECK(output1.str() == "7/15");
+
+		CRational rational2(-7, 15);
+		std::stringstream output2;
+		output2 << rational2;
+		CHECK(output2.str() == "-7/15");
+
+		CRational rational3(7);
+		std::stringstream output3;
+		output3 << rational3;
+		CHECK(output3.str() == "7/1");
+
+		std::stringstream output4;
+		output4 << CRational();
+		CHECK(output4.str() == "0/1");
+	}
+
+	SECTION("Operator >>")
+	{
+		CRational rational1;
+		std::stringstream input1("7/15");
+		input1 >> rational1;
+
+		CHECK(rational1.GetNumerator() == 7);
+		CHECK(rational1.GetDenominator() == 15);
+
+		CRational rational2;
+		std::stringstream input2("-7/15");
+		input2 >> rational2;
+		CHECK(rational2.GetNumerator() == -7);
+		CHECK(rational2.GetDenominator() == 15);
+
+		CRational rational3;
+		std::stringstream input3("test");
+		CHECK_THROWS_AS(input3 >> rational3, std::invalid_argument);
+
+		CRational rational4;
+		std::stringstream input4("3/1");
+		input4 >> rational4;
+		CHECK(rational4.GetNumerator() == 3);
+		CHECK(rational4.GetDenominator() == 1);
 	}
 }
